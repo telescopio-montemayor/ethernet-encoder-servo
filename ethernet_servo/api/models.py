d3272801162669945e177e5adde83af15b41b7c7
Adrián Pardini <publico@tangopardo.com.ar>
2019-04-19 09:56:50 -0300

    api: adds ControllerState model
    this is more correct-ish than using DeviceStatus. We'll cleanup the api
    later

--- ethernet_servo/api/models.py
+++ ethernet_servo/api/models.py
@@ -57,10 +57,22 @@ DeviceStatus = api.model('DeviceStatus', {
     'position_angle': fields.Nested(model=AnglePosition, attribute='controller.state.position_angle'),
     'position_astronomical': fields.Nested(model=AstronomicalPosition, attribute='controller.state.position_astronomical'),
     'error': fields.Float(attribute='controller.state.error'),
-    'offset': fields.Integer(attribute='controller.state.offset'),
-    'output': fields.Float(attribute='controller.state.output'),
-    'slew_rate_limit': fields.Float(attribute='controller.state.slew_rate_limit'),
-    'Kp': fields.Float(attribute='controller.state.Kp'),
-    'Ki': fields.Float(attribute='controller.state.Ki'),
-    'Kd': fields.Float(attribute='controller.state.Kd'),
+})
+
+ControllerState = api.model('ControllerState', {
+    'offset': fields.Integer(attribute='offset', default=0),
+    'error': fields.Float(attribute='error', default=0),
+    'position': fields.Float(attribute='position', default=0),
+    'output': fields.Float(attribute='output', default=0),
+    'slew_rate_limit': fields.Float(attribute='slew_rate_limit', default=12000),
+    'alpha': fields.Float(attribute='alpha', default=0.75),
+    'Kp': fields.Float(attribute='Kp', default=1.8),
+    'Ki': fields.Float(attribute='Ki', default=1),
+    'Kd': fields.Float(attribute='Kd', default=1),
+    # 'invert': fields.Boolean(attribute='device.invert'),
+    'tracking': fields.Boolean(attribute='tracking', default=False),
+    'free_running': fields.Boolean(attribute='free_running', default=False),
+    'run_speed': fields.Nested(model=AnglePosition, attribute='run_speed'),
+    'closed_loop': fields.Boolean(attribute='closed_loop', default=False),
+    'target': fields.Float(attribute='target'),
 })
